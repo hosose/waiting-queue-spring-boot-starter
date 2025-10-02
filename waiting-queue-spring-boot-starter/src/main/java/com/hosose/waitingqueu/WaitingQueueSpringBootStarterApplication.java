1ef3ad82639ec7c4a8ad63996659a0ce5c880553
hosose <hosose22@gmail.com>
2025-10-02 14:27:06 +0900

    initial commit

---
+++ waiting-queue-spring-boot-starter/src/main/java/com/hosose/waitingqueu/WaitingQueueSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.hosose.waitingqueu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingQueueSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingQueueSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ waiting-queue-spring-boot-starter/src/test/java/com/hosose/waitingqueu/WaitingQueueSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosose.waitingqueu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingQueueSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
