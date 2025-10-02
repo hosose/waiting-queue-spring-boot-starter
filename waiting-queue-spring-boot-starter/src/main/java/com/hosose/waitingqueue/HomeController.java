4541bf16e3928084bb50b70ac8d7d7bbb9e455aa
hosose <hosose22@gmail.com>
2025-10-02 15:05:03 +0900

    테스트용 화면 생성 및 설정 파일 수정

---
+++ waiting-queue-spring-boot-starter/src/main/java/com/hosose/waitingqueue/HomeController.java
@@ -0,0 +1,12 @@
+package com.hosose.waitingqueue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "서비스에 오신 것을 환영합니다!";
+    }
+}
